cc07ee35385be745d330c906514a0d59dbfa7a4c
Ivanarre <arre2397@gmail.com>
2025-12-13 18:53:48 +0800

    tailwind and prettier setup

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -10,7 +10,7 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div>
       {isLoading && <Loader />}
 
       <Header />
